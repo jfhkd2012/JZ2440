b54dc4d4ec2e9092ed44d58abc01639a54a9b0be
jf-home-pc <jfhkd2012@126.com>
2016-10-14 20:51:54 +0800

    finish first stage

--- ramdisk/ramdisk.c
+++ ramdisk/ramdisk.c
@@ -25,6 +25,22 @@
 #include <asm/dma.h>
 
 static struct gendisk *ramblock_disk;
+static request_queue_t *ramblock_queue;
+
+static DEFINE_SPINLOCK(ramblock_lock);
+static int major;
+
+static struct block_device_operations ramblock_fops = {
+	.owner	=	THIS_MODULE,
+};
+
+#define RAMBLOCK_SIZE		(1024*1024)
+
+static void do_ramblock_request(request_queue_t *q)
+{
+	static int cnt = 0;
+	printk("do ramblock_request %d\n", cnt++);
+}
 
 static int ramdisk_init(void)
 {
@@ -32,16 +48,28 @@ static int ramdisk_init(void)
 	ramblock_disk = alloc_disk(16);  /* 次设备号个数:  分区个数+1 */
 	/* 2、 设置*/
 	/* 2.1、设置/分配一个队列: 提供读写能力*/
-
+	ramblock_queue = blk_init_queue(do_ramblock_request, &ramblock_lock);
+	ramblock_disk->queue = ramblock_queue;
+	
 	/* 2.2、设置其他属性，比如容量等*/
+	major = register_blkdev(0, "ramblock");
+	ramblock_disk->major = major;
+	ramblock_disk->first_minor = 0;
+	sprintf(ramblock_disk->disk_name, "ramblock");
+	ramblock_disk->fops = &ramblock_fops;
+	set_capacity(ramblock_disk, RAMBLOCK_SIZE/512);
 
 	/* 3、注册 */
+	add_disk(ramblock_disk);
 	return 0;
 }
 
 static void ramdisk_exit(void)
 {
-
+	unregister_blkdev(major, "ramblock");
+	del_gendisk(ramblock_disk);
+	put_disk(ramblock_disk);
+	blk_cleanup_queue(ramblock_queue);
 }
 
 module_init(ramdisk_init);
