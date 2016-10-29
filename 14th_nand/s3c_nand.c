bfabeb0fd2265ff5c4d56c87ad3399cea27d3ce3
jf-home-pc <jfhkd2012@126.com>
2016-10-29 21:36:33 +0800

    finish nand flash driver 1th

--- 14th_nand/s3c_nand.c
+++ 14th_nand/s3c_nand.c
@@ -26,4 +26,68 @@ static struct nand_chip *s3c_nand;
 static struct mtd_info *s3c_mtd;
 
 
+static void s3c2440_select_chip(struct mtd_info *mtd, int chipnr)
+{
+	if(chipnr == -1)
+	{
+		/* 取消选中: NFCONT[1] 设为0 */
+	}
+	else
+	{
+		/* 选中: NFCONT[1] 设为1*/
+	}
+}
+
+static void s3c2440_cmd_ctrl(struct mtd_info *mtd, int dat, unsigned int ctrl)
+{
+	if(ctrl & NAND_CLE)
+	{
+		/* Send Command : NFCMMD = dat*/
+	}
+	else
+	{
+		/* Send Address : NFADDR = dat */
+	}
+}
+
+static int s3c2440_dev_ready(struct mtd_info * mtd)
+{
+	return "bit0 of NFSTAT"
+}
+static int s3c_nand_init(void)
+{
+	/* 1、 分配一个nand_chip 结构体*/
+	s3c_nand = kzalloc(sizeof(struct nand_chip), GFP_KERNEL);
+
+	/* 2、设置nand_chip */
+	/* 设置nand_chip 是给nand_scan函数使用的，如果不知道怎么设置，先看nand_scan怎么使用的
+	 *  它提供: 选中，发命令，发地址，发数据，读数据，判断状态的功能
+	*/
+	s3c_nand->select_chip      = s3c2440_select_chip;
+	s3c_nand->cmd_ctrl         = s3c2440_cmd_ctrl;
+	s3c_nand->IO_ADDR_R    = "NFDATA Virtual Address";
+	s3c_nand->IO_ADDR_W   = "NFDATA Virtual Address";
+	s3c_nand->dev_ready       = s3c2440_dev_ready;
+
+	/* 3、硬件相关的设置*/
+
+	/* 4、使用：nand_scan */
+	s3c_mtd = kzalloc(sizeof(struct mtd_info), GFP_KERNEL);
+	s3c_mtd->owner = THIS_MODULE;
+	s3c_mtd->priv    = s3c_nand;
+
+	nand_scan(s3c_mtd, 1);  /* 识别NAND FLASH, 构造mtd_info */
+
+	/* 5、add_mtd_partitions */
+	return 0;
+}
+
+static void s3c_nand_exit(void)
+{
+
+}
+
+module_init(s3c_nand_init);
+module_exit(s3c_nand_exit);
+MODULE_LICENSE("GPL");
 
